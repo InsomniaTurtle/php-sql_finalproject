4bb1652908d8f05821435fdc1505c361fcd60228
Colin Young <c.young19@students.clark.edu>
2020-06-02 09:47:04 -0700

    version 0.5

---
+++ inc/functions.inc.php
@@ -0,0 +1,12 @@
+<!-- function area -->
+<?php
+
+?>
+<!-- recipe typing area -->
+<?php
+if (isset($_GET['bake'])) {
+    // if user chooses bake puts a whisk image next to post @ username
+} elseif (isset($_GET['cook'])) {
+    // if the user selected the other option then put a knife image instead
+};
+?>
